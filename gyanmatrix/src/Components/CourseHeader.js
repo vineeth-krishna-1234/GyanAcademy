f500752317d977390df4f802e3704ba96148b120
DHARUNDP5 <dharunprakashkdp@gmail.com>
2022-11-04 11:51:11 +0530

    courseLibrary page Css [Dharun]

---
+++ gyanmatrix/src/Components/CourseHeader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function CourseHeader() {
+  return (
+    <div>
+      {" "}
+      <div className="flex  flex-row justify-start mt-[24px] w-[1162px] border-b-[1px] h-[40px]">
+        {["Most Popular", "Highly Rated", "New"].map((obj) => (
+          <span className="ml-12 h-full border-b-[2px] border-[#9D1C7F] p-[5px]">
+            <h3>{obj}</h3>
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CourseHeader
--- gyanmatrix/src/CoursePages/coursePage/coursePage.js
+++ gyanmatrix/src/CoursePages/coursePage/coursePage.js
@@ -10,7 +10,7 @@ import bgImg from "./asserts/Banner.svg"
 
 
 function CoursePage() {
-  const Text = "JavaScript";
+  const Text = "JavaScript";   
   return (
     <div>
       <div>
